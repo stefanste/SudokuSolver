a6b6ae52449a08f57b72518ccfd4001035a4f95e
Stefan Kojic <stefan.kojic@fundingcircle.com>
2020-01-29 19:13:09 +1030

    Upgrade to Ruby 2.6.5, Rails 5.0

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.2.0'
+ruby '2.6.5'
 
 gem 'twitter', '~> 5.14.0'
 gem 'pg'
@@ -8,13 +8,15 @@ gem 'method_profiler', '~> 2.0.1'
 gem 'ruby-prof', '~> 0.15.6'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.0'
+gem 'rails', '~> 5.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
+
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
+#gem 'coffee-rails', '~> 4.1.0'
+
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
@@ -48,4 +50,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -21,18 +20,16 @@ ActiveRecord::Schema.define(version: 20150308064056) do
     t.integer  "tweet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["tweet_id"], name: "index_replies_on_tweet_id", using: :btree
   end
-
-  add_index "replies", ["tweet_id"], name: "index_replies_on_tweet_id", using: :btree
 
   create_table "tweets", force: :cascade do |t|
     t.string   "fulltext"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id", using: :btree
   end
-
-  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "account_name"
